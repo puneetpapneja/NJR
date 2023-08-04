209749c61af9a677b61f25221e4fe72a51be4f07
suthararvind <suthararvindjlr757@gmail.com>
2023-08-04 02:31:01 -0700

    added old assginments

---
+++ assignments/03.08.23/Myscript.js
@@ -0,0 +1,23 @@
+var x=10;
+let y=10.25;
+x+=y;
+document.write(x);
+document.write("<br>");
+
+x**=y;
+document.write(x);
+
+
+document.write("<br>");
+
+
+var temp="Arvind";
+var test="Suthar";
+temp+=test;
+document.write(temp);
+
+
+
+
+
+
