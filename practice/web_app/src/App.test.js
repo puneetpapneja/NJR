1e0dc917553091a78b31bc4d1a61b2e703ed0049
vedas-dixit <vedasdixit1006@gmail.com>
2023-08-05 09:44:28 +0530

    new web-app commit

--- practice/web_app/src/App.test.js
+++ practice/web_app/src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  render(<new />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
