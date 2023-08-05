858e252b1ce20ff6a3aaf0668043fea8902323b5
sk-pathan <shoaibkp313@gmail.com>
2023-08-05 15:49:23 +0530

    added some change into project

---
+++ practice/reactapp/src/component/Iteam.js
@@ -0,0 +1,10 @@
+import"./Iteam.css";
+export const Iteam=()=>{
+    return(
+        <div id="table">
+            <div class="text">brand</div>
+            <div class="text">iteam</div>
+            <div class="text">price</div>
+        </div>
+    )
+}
