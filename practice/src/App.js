963ddb75efebce089819c4e34aa8296356d49d61
Rupalsoni123 <rupalsoni518@gmail.com>
2023-08-08 00:21:01 +0530

    assigment 07-08-23

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,41 +1,26 @@
 import './App.css';
 import Item from './components/Item';
-
+import Assign1 from './components/Assign1';
 function App() {
-  const items=[
-    {
-        item:"Bottle",
-        brand:"cello",
-        price:"115"
-    },
-    {
-        item:"Bottle",
-        brand:"Montex",
-        price:"50"
-    },
-    {
-        item:"Bottle",
-        brand:"Milton",
-        price:"300"
-    },
-    {
-        item:"Bottle",
-        brand:"Tupperware",
-        price:"100"
-    },
-    {
-        item:"Bottle",
-        brand:"Bellon",
-        price:"200"
-    }
-]
+const carData = {
+  car1: {
+    name: "Car Model 1",
+    image: "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Hyundai-Grand-i10-Nios-200120231541.jpg&w=872&h=578&q=75&c=1",
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    specifications: ["Spec 1: Value", "Spec 2: Value", "Spec 3: Value"],
+  },
+  car2: {
+    name: "Car Model 2",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCya7c94Klx4C8t86wFT44YivG2otS-Ymkj2TocD_&s",
+    features: ["Feature A", "Feature B", "Feature C"],
+    specifications: ["Spec A: Value", "Spec B: Value", "Spec C: Value"],
+  },
+};
   return (
     <div className="App">
-<Item items={items}></Item>
 <Item></Item>
-<Item></Item>
-<Item></Item>
-<Item></Item>
+<Assign1 car={carData.car1}></Assign1>
+<Assign1 car={carData.car2}></Assign1>
     </div>
   );
 }
--- practice/src/components/Item.js
+++ practice/src/components/Item.js
@@ -1,10 +1,14 @@
 import './item.css';
-const Item=(props)=>{
+    const Item = (props)=>{
     return (
     <div className="container">
+    
         <div className="colum1">Bottle</div>
         <div className="colum1">Cello</div>
-        <div className="colum1">115</div>
+        <div className="colum1">
+      115
+        </div>
+
     </div>
     )
 }
