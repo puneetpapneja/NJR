95b95e4d7b84667a16f45c88eed23f68eed0defd
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-08-07 11:26:06 +0530

    files added

--- practice/webapp/src/index.js
+++ practice/webapp/src/index.js
@@ -7,9 +7,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />   
   </React.StrictMode>
 );
+ //used App component as a tag
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
