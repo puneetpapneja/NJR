83a4496c125fc3db16775a81ea6b9125a06227cc
saurabh-s19 <saurabhsoni7014@gmail.com>
2023-08-07 11:26:26 +0530

    ver 2.0

---
+++ practice/my-app/src/component/Item.js
@@ -0,0 +1,30 @@
+import React  from "react";
+import "./Item.css";
+
+const Item=()=>{
+    const items=[
+        {
+            item:"bottle",
+            brand:"cello",
+            price:"123 rupees"
+        }
+        {
+            item:"boat",
+            brand:"cel",
+            price:"234 rupees"
+        }
+        {
+            item:"goat",
+            brand:"cel",
+            price:"345 rupees"
+        }
+    ]
+    return(items?.map()
+  <div>bottle</div>
+  <div>cello</div>
+  <div>150 ruppes</div>
+
+    </div> );
+}
+
+export default Item;
