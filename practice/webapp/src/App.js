3f1d41745b4a1aea5ebba0852aa3d439c116b18c
sahil19solanki <sahil19solanki@gmail.com>
2023-08-07 11:27:01 +0530

    before adding props

---
+++ practice/webapp/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Item from "./componants/Item";
+const items = [
+  {
+    item: "Bottle",
+    brand: "Cello",
+    price: 115,
+  },
+  {
+    item: "Mobile",
+    brand: "Mi",
+    price: 100000,
+  },
+  {
+    item: "Mac",
+    brand: "Apple",
+    price: 99,
+  },
+];
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ practice/webapp/src/componants/Item.js
@@ -0,0 +1,8 @@
+import "./Item.css";
+const Item = () => {
+  
+  return <div>
+
+  </div>;
+};
+export default Item;
