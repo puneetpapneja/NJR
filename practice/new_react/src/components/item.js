0c2548c021cd0f9ddf04c3f218de9f7302c3e62f
Vaibhav <vaibhavgarg977@gmail.com>
2023-08-07 13:41:12 +0530

    next reat update

---
+++ practice/new_react/src/components/item.js
@@ -0,0 +1,18 @@
+import "./item.css";
+const Item = () =>{
+    return (<div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Item</th>
+                    <th>Brand</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+        </table>
+        
+    </div>
+    );
+}
+
+export default Item;
