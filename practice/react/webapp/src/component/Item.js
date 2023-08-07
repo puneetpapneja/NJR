27545984fbbced6684f2a1b1e02d7b110a42858d
AasthaDakhera <aastha.dakhera@gmail.com>
2023-08-07 11:39:28 +0530

    added practice files

---
+++ practice/react/webapp/src/component/Item.js
@@ -0,0 +1,28 @@
+import "./item.css"
+export default function Item() {
+    return (
+        <div className="div_b">
+            <section>Bottle</section>
+            <section>Cello</section>
+            <section id="price">115</section>
+        </div>
+
+    );
+}
+const Items=[{
+    item: "Bottle",
+    brand: "cello",
+    price: 115
+},
+{
+    item: "Bottle",
+    brand: "cello",
+    price: 115
+},
+{
+    item: "Bottle",
+    brand: "cello",
+    price: 115
+}
+];
+   
