787f2197e1ad84362d6ce51e5427d5732067f73c
tanishakumawat <113818130+tanishakumawat@users.noreply.github.com>
2023-08-08 13:07:27 +0530

    modified:   practice/src/index.js

--- practice/src/index.js
+++ practice/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
