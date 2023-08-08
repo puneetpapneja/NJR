3e2ee468285181753867b396b832bdcbd3a27c0e
sudeep-roy <rsudeep869@gmail.com>
2023-08-08 13:38:42 +0530

    Commited

---
+++ practice/project1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Item>
+    </Item>
+     </div>
+  );
+}
+
+export default App;
+
---
+++ practice/project1/src/components/item.js
@@ -0,0 +1,19 @@
+const item = ()=>{
+    return(<div>
+        <table>
+            <tr>
+                <th>Item</th>
+                <th>Brand</th>
+                <th>Price</th>
+            </tr>
+            <body>
+                <tr>
+                    <td>Bottle</td>
+                    <td>Cello</td>
+                    <td>110</td>
+                </tr>
+            </body>
+        </table>
+    </div>);
+}
+export default item;
