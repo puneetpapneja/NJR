7009e4ab709db9f56fb799743eea7ca9536d521c
sumit.vasita <sumitvasita5@gmail.com>
2023-08-08 13:50:42 +0530

    changes

---
+++ practice/sumit/src/components/item.js
@@ -0,0 +1,19 @@
+import './Item.css';
+const Item = ()=>{
+    return (<div>
+        <table>
+            <thead>
+            <tr>
+                <th>Item</th>
+                <th>brand</th>
+                <th>price</th>
+            </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+         </div>);
+
+}
+export default Item;
