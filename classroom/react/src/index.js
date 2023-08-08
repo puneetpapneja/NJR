58830679a325434bc7be0c603f9598e4c05b1370
xowais07 <owaiskhanudpr@gmail.com>
2023-08-08 14:48:15 +0530

    hy

--- classroom/react/src/index.js
+++ classroom/react/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
+//bootstrap bundle js
+import "bootstrap/dist/js/bootstrap.min";
+
 // test()
 // test1();
 const root = ReactDOM.createRoot(document.getElementById('root'));
