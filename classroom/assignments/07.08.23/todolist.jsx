cdb015e3794326a56ab88bd74a311f193984145b
Bheru Singh <bherusinghpanwar43@gmail.com>
2023-08-08 21:48:55 +0530

    todolist with delete

--- classroom/assignments/07.08.23/todolist.jsx
+++ classroom/assignments/07.08.23/todolist.jsx
@@ -1,27 +1,40 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const ToDoList = () => {
   const [inputValue, setInputValue] = useState("");
-  const [todoItem, settodoItem] = useState([]);
+  const [todoItems, setTodoItems] = useState([]);
+
   function handleChange(event) {
     setInputValue(event.target.value);
   }
+
   function handleSubmit() {
     if (!inputValue) {
       return;
     } else {
-      settodoItem([...todoItem, inputValue]);
+      setTodoItems([...todoItems, inputValue]);
       setInputValue("");
     }
   }
+
+  function handleDelete(index) {
+    const newArr = todoItems.filter((_, i) => i !== index);
+    setTodoItems(newArr);
+  }
+
   return (
     <div>
       <h1>Todo List:</h1>
       <input type="text" onChange={handleChange} value={inputValue} />
       <button onClick={handleSubmit}>Click me</button>
-      {todoItem.map((items) => (
-        <li>{items}</li>
-      ))}
+      <ul>
+        {todoItems.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
