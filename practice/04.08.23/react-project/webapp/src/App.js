e0f01a76b0400b5e7fdea81493386a03162552f3
Khushiv0511 <khushivanawat.2021cs@technonjr.org>
2023-08-08 10:59:36 +0530

    message

--- practice/04.08.23/react-project/webapp/src/App.js
+++ practice/04.08.23/react-project/webapp/src/App.js
@@ -1,23 +1,7 @@
 
 import './App.css';
 import Item from './component/Item';
-const items =[
-  {
-      item:"Bottle"
-      brand:"cello"
-      price:115
-  },
-  {
-      item:"Bottle"
-      brand:"cello"
-      price:115
-  },
-  {
-      item:"Bottle"
-      brand:"cello"
-      price:115
-  }
-]
+
 function App() {
   return (
     <div className="App">
