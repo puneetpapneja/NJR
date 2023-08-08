c7efb50452706d9388a5ba49c47e716c02cc3333
yuvraj-singhkanawat <yuvrajskanawat@gmail.com>
2023-08-08 13:20:08 +0530

    changes

--- practice/project1/src/components/item.js
+++ practice/project1/src/components/item.js
@@ -24,31 +24,31 @@
 //                 </table>}
 //                 </div>
 // }
-import './item.css';
-const Item =()=>{
-    const items = [
-        {
-            item:"Bottle",
-            brand: "cello" ,
-            price :  200
-        },
-        {
-            item:"Mobile",
-            brand: "Moto" ,
-            price :  15000
-        },
-        {
-            item:"Iphone 5s",
-            brand: "Iphone" ,
-            price :  2600
-        },
+// import './item.css';
+// const Item =()=>{
+//     const items = [
+//         {
+//             item:"Bottle",
+//             brand: "cello" ,
+//             price :  200
+//         },
+//         {
+//             item:"Mobile",
+//             brand: "Moto" ,
+//             price :  15000
+//         },
+//         {
+//             item:"Iphone 5s",
+//             brand: "Iphone" ,
+//             price :  2600
+//         },
         
-    ]
-    return <div>
-        <table>
-            <thead>
+//     ]
+//     return <div>
+//         <table>
+//             <thead>
                 
-            </thead>
-        </table>
-    </div>
-}
+//             </thead>
+//         </table>
+//     </div>
+// }
