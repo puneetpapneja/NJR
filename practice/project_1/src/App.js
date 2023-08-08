ae94a1999f5baf0492da89e5e777f5ffb8b42b46
neelamkataria24 <neelamkatariya2003@gmail.com>
2023-08-08 12:28:17 +0530

    fixed commit

--- practice/project_1/src/App.js
+++ practice/project_1/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './components/Item';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+     <div className="App">
+      <Item/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ practice/project_1/src/components/Item.js
@@ -0,0 +1,3 @@
+import Item from './Item.css';
+const Item = () => <h1>Item components</h1>;
+
