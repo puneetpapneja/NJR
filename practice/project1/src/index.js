d0c7b08df9e4efeaf09dff8f31c3325567dd538d
yuvraj-singhkanawat <yuvrajskanawat@gmail.com>
2023-08-08 14:18:54 +0530

    bootstrap added

--- practice/project1/src/index.js
+++ practice/project1/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
