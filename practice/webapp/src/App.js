5cce4524b29d8126b8b5e7ede8764302f7226ce4
Khushigahlot29 <gahlotkhushi5@gmail.com>
2023-08-08 12:39:18 +0530

    fixed folder structure

---
+++ practice/webapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Item from './component/Item';
+function App() {
+  return  (<div>
+    <Item/>
+    
+    </div>
+  );
+}
+
+export default App;
