c734d34173bd4ecd793f2ae8e49d72a8b0b48609
sumit.vasita <sumitvasita5@gmail.com>
2023-08-08 14:20:41 +0530

    changes

--- practice/sumit/src/index.js
+++ practice/sumit/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
