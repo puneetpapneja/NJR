a3ae1198d01f0732ccf3fb5ab52fe03ef529c7e2
Garvita18 <garvitabaya.2021cs@technonjr.org>
2023-08-09 09:56:26 +0530

    modified:   .gitignore 	new file:   practice/04.08.23/react-project/package.json 	new file:   practice/04.08.23/react-project/public/favicon.ico 	new file:   practice/04.08.23/react-project/public/index.html 	new file:   practice/04.08.23/react-project/package-lock.json 	new file:   practice/04.08.23/react-project/public/logo192.png 	new file:   practice/04.08.23/react-project/public/logo512.png 	new file:   practice/04.08.23/react-project/public/manifest.json 	new file:   practice/04.08.23/react-project/public/robots.txt 	new file:   practice/04.08.23/react-project/src/App.css 	new file:   practice/04.08.23/react-project/src/App.js 	new file:   practice/04.08.23/react-project/src/App.test.js 	new file:   practice/04.08.23/react-project/src/components/Item.css 	new file:   practice/04.08.23/react-project/src/components/Item.js 	new file:   practice/04.08.23/react-project/src/index.css 	new file:   practice/04.08.23/react-project/src/index.js 	new file:   practice/04.08.23/react-project/src/logo.svg 	new file:   practice/04.08.23/react-project/src/reportWebVitals.js 	new file:   practice/04.08.23/react-project/src/setupTests.js 	new file:   practice/package-lock.json

---
+++ practice/04.08.23/react-project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Item from './components/Item';
+
+function App() {
+  return (
+    <div>
+      <Item x='sattle'/>
+      <Item x='cello'/>
+      <Item x='115'/>
+
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ practice/04.08.23/react-project/src/components/Item.js
@@ -0,0 +1,15 @@
+// import './Item.css';
+// const Item=()=>{
+//     return( <h1>
+//         Item components
+//         </h1>);
+// }
+// export default Item;
+
+import React from 'react'
+export default function Item({x}){
+    return(
+        <div>{x}</div>
+    )
+} 
+
