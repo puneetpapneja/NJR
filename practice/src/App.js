a1cbd8131d6e4d6b343747bd3f7dcd0fe6cdad61
jigyasac07 <jigyasachaturvedi.2021cs@technonjr.org>
2023-08-09 11:22:59 +0530

    modified:   .gitignore 	new file:   practice/.gitignore 	new file:   practice/README.md 	new file:   practice/package-lock.json 	new file:   practice/package.json 	new file:   practice/public/favicon.ico 	new file:   practice/public/index.html 	new file:   practice/public/logo192.png 	new file:   practice/public/logo512.png 	new file:   practice/public/manifest.json 	new file:   practice/public/robots.txt 	new file:   practice/src/App.css 	new file:   practice/src/App.js 	new file:   practice/src/App.test.js 	new file:   practice/src/component/Item.css 	new file:   practice/src/component/Item.js 	new file:   practice/src/index.css 	new file:   practice/src/index.js 	new file:   practice/src/logo.svg 	new file:   practice/src/reportWebVitals.js 	new file:   practice/src/setupTests.js

---
+++ practice/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+import Item  from './component/Item';
+function App() {
+  return (
+    <div className="App">
+   <Item x="bottle"></Item>
+   <Item y= "cello"></Item>
+   <Item z="155"></Item>
+
+   <Item x="bottle"></Item>
+   <Item y= "cello"></Item>
+   <Item z="155"></Item>
+   
+   <Item x="bottle"></Item>
+   <Item y= "cello"></Item>
+   <Item z="155"></Item>
+     </div>
+  );
+}
+
+export default App;
---
+++ practice/src/component/Item.js
@@ -0,0 +1,22 @@
+import './Item.css';
+const Item = () =>[
+    {
+        item: "bottle",
+        brand:"cello",
+        price:"115",
+    },
+    {
+        item: "phone",
+        brand:"vivo",
+        price:"11500",
+    }
+]
+        return(
+        <div className="container">
+            <div className="column1">Bottle</div>
+            <div className="column1">Cello</div>
+            <div className="column1">115</div>
+            </div>
+        );
+       
+export default Item;
