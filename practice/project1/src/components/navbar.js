6fe6422244fd8673bee8412faa1cddabb45cab68
yuvraj-singhkanawat <yuvrajskanawat@gmail.com>
2023-08-09 13:26:22 +0530

    new files  added

--- practice/project1/src/components/navbar.js
+++ practice/project1/src/components/navbar.js
@@ -1,23 +1,36 @@
-const NavBar = () => {
-    return (  <Nav
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import {Link} from 'react-router-dom';
+
+const NavBar = () =>{
+    return (
+    // <Navbar expand="lg" className="bg-body-tertiary">
+    //   <Container fluid>
+    //     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className="me-auto">
+    //         <Nav.Link as={Link} to="/">Home</Nav.Link>
+    //         <Nav.Link as={Link} to="/about">about us</Nav.Link>
+    //         <Nav.Link as={Link} to="/contact">Contact us</Nav.Link>
+    <Navbar expand="lg" className="bg-body-tertiary">
+    <Container fluid>
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <link to="/">Home</link>
+        <link to="/about">Aboutus</link>
+        <link to="/contact">Contactus</link>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/about">about us</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact us</Nav.Link>
+
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
-  }
+}
+
 export default NavBar;
