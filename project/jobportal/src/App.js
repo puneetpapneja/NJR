4332b927031db1592aaa5d8343eda6efc70aa6b9
Ace7568 <jayjoshi.2021cs@technonjr.org>
2023-08-09 12:12:18 +0530

    modified:   project/jobportal/package-lock.json 	modified:   project/jobportal/package.json 	modified:   project/jobportal/src/App.js 	new file:   project/jobportal/src/components/login.jsx 	new file:   project/jobportal/src/pages/login.js

--- project/jobportal/src/App.js
+++ project/jobportal/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+// import Button from 'react-bootstrap/esm/Button';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <Button variant="secondary">Secondary</Button>{' '} */}
     </div>
   );
 }
---
+++ project/jobportal/src/pages/login.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Login } from '../components/login'
+
+export const loginPage = () => {
+  return  <Login/>
+}
