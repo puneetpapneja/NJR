5794fdce951836509297c9ca31b75ea9edab448c
Vishal kumawat <kumawatvishal0376@gmail.com>
2023-08-10 18:28:31 +0530

    recent update

--- practice/project/src/App.js
+++ practice/project/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Navbar from './components/Navbar.js';
+import Navbar from './components/Navbar.js';
 import Login from "./components/login.js";
 import Register from "./components/Register.js";
 
@@ -8,7 +8,7 @@ import Register from "./components/Register.js";
 function App() {
   return (
     <>
-    {/* <Navbar /> */}
+    <Navbar />
     <Login/>
     <Register/>
     </>
--- practice/project/src/components/Navbar.js
+++ practice/project/src/components/Navbar.js
@@ -6,7 +6,6 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
