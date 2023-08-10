ca6ee1e22f2644d69c626ed9a7d18e2eff40178c
AasthaDakhera <aastha.dakhera@gmail.com>
2023-08-10 09:46:10 +0530

    added react project

--- practice/react/webapp/src/App.js
+++ practice/react/webapp/src/App.js
@@ -1,34 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
-import Item from "./component/Item";
-const Items1=[{
-  item: "tiffin",
-  brand: "cello",
-  price: 115
-},
-{
-  item: "Bottle",
-  brand: "cello",
-  price: 115
-},
-{
-  item: "Bottle",
-  brand: "cello",
-  price: 115
-}
-];
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { routes } from './routing';
+
  
 
 
 
 function App() {
   return (
-    <div >
-    <Item item="bottle" brand="cello" price={115}/>
-    <Item item="tiffin" brand="cello" price={200}/>
-    <Item list={Items1}/>
-    
-    </div>
+    <RouterProvider router={createBrowserRouter(routes)}>
+    </RouterProvider>
   );
 }
 
---
+++ project/jobportal/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './pages/loginPage';
+
+function App() {
+  return (
+    <LoginPage/>
+  );
+}
+
+export default App;
