634b82e2736f45f27a656a0b8d2f2931d1fb7b0b
जय जिनेंद्र <lokantik>
2023-08-10 12:07:03 +0530

    changes

--- classroom/react/src/App.js
+++ classroom/react/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import React, { useState } from 'react';
 import ToDoList from './components/ToDoList';
 import Header from './components/Header';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import Home from './components/Home';
-=======
-import React from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
->>>>>>> 05ebde8791f651ead5cccd711d676e201d42d6e5
 import { routes } from './routing';
 
 
--- classroom/react/src/routing.js
+++ classroom/react/src/routing.js
@@ -4,12 +4,11 @@ import Aboutus from './components/About';
 import Contactus from './components/Contact';
 import Header from './components/Header';
 import PageLayout from './components/page-layout';
-<<<<<<< HEAD
+
 import ToDoList from './components/ToDoList';
-=======
 import Memo from './components/Memo';
-import Item from './components/Item';
->>>>>>> 05ebde8791f651ead5cccd711d676e201d42d6e5
+import item from './components/item';
+
 export const routing = [
     {
         path: "/",
@@ -22,46 +21,23 @@ export const routing = [
     {
         path: "/contact",
         element:  <Contactus />
-<<<<<<< HEAD
-    },{
-        path: "/about",
-        element:  <Aboutus />
-    },{
-        path: "/about",
-        element:  <Aboutus />
-    },{
-        path: "/about",
-        element:  <Aboutus />
-    },{
-        path: "/about",
-        element:  <Aboutus />
-    },{
-        path: "/about",
-        element:  <Aboutus />
-    },{
-        path: "/about",
-        element:  <Aboutus />
-    },{
-        path: "/about",
-        element:  <Aboutus />
-    },{
-        path: "/about",
-        element:  <Aboutus />
-    },{
-        path: "/about",
-        element:  <Aboutus />
+
     },
-=======
-    },
+    
     {
         path: "/usememo",
         element:  <Memo />
     },
     {
         path: "/item",
-        element:  <Item list={[{item: "Bottle", brand:"Cello", price:115}]} heading="Ceramic Products" />
+        element:  <Item />
+    },
+
+    {
+        path: "/ToDoList",
+        element: < ToDoList/>
     }
->>>>>>> 05ebde8791f651ead5cccd711d676e201d42d6e5
+
 ]
 
 export const routes = [
