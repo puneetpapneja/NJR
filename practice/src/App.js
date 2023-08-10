5c6bd82e637c72dbec1c4ca664274dca076fe75c
arun12329 <132123854+arun12329@users.noreply.github.com>
2023-08-10 11:15:37 +0530

    Changes to be committed: 	modified:   .gitignore 	deleted:    README.md 	modified:   classroom/react/package-lock.json 	modified:   practice/src/App.css 	modified:   practice/src/App.js 	modified:   practice/src/index.css

--- practice/src/App.js
+++ practice/src/App.js
@@ -19,8 +19,9 @@ function App() {
        return <Item key={e.name} x={e.name} clr={e.clr}/>
       })}
     </div>
-   
-    <img src="https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80" id='image' alt='afd' />
+     <div id="img2">
+    <img  id="image" src="https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"  alt='afd' />
+    </div>
     </>
   );
 }
