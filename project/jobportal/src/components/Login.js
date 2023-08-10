7a946124eccc7e18e711fbfbfa72899a199d8545
Dhirensuhalka <suhalka.dhiren07@gmail.com>
2023-08-10 11:50:04 +0530

    changes

---
+++ project/jobportal/src/components/Login.js
@@ -0,0 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Login.css';
+function BasicExample() {
+  return (
+    <>
+     <h1>Login Page</h1>
+    <Form className="form">
+       
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control className="input" type="email" placeholder="Enter email" />
+        
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control className="input" type="password" placeholder="Password" />
+      </Form.Group>
+      <div>
+      <a href='/'>Don't have a account?Register Now</a>
+      </div>
+      <Button className="btn" variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </>
+  );
+}
+export default BasicExample
