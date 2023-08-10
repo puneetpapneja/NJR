53d199a6de7996de0a31ce923385de08c3b382a4
Vishal kumawat <kumawatvishal0376@gmail.com>
2023-08-10 20:48:45 +0530

    d

--- classroom/react/src/components/Hooks.js
+++ classroom/react/src/components/Hooks.js
@@ -1,35 +1,35 @@
 import { useEffect, useState } from "react";
 import { Button } from 'react-bootstrap';
 
-const Hooks = ()=>{
-    const [value, changeValue] = useState();
-  useEffect(()=> {
+const Hooks = () => {
+  const [value, changeValue] = useState();
+  useEffect(() => {
     console.log("Load 1 time only when component load")
-  },[])
+  }, [])
 
- 
-  useEffect(()=> { 
+
+  useEffect(() => {
     console.log("value state change")
-  },[value]); 
+  }, [value]);
 
-  useEffect(()=> { 
+  useEffect(() => {
     console.log("useEffect trigger everytime")
   });
 
-  useEffect(()=> { 
-   return () => {
-    console.log("hooks component unload");
-   }
+  useEffect(() => {
+    return () => {
+      console.log("hooks component unload");
+    }
   });
 
- 
-    return (
-        <div>
-            <h1>Hooks examples</h1>
-            Value : {value}
-            <Button variant="primary">Primary</Button>
-        </div>
-    )
+
+  return (
+    <div>
+      <h1>Hooks examples</h1>
+      Value : {value}
+      <Button variant="primary">Primary</Button>
+    </div>
+  )
 }
 
 
