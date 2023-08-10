969f49f0e41eca9e2e8452c2ceafa0e27bf72069
saurabh-s19 <saurabhsoni7014@gmail.com>
2023-08-10 09:47:23 +0530

    add 10.08.23

---
+++ assignments/07.08.23/todo-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Todo from "./component/Todo"
+
+function App() {
+  return (
+   
+    
+      <Todo/>
+    
+  );
+}
+
+export default App;
---
+++ assignments/07.08.23/todo-app/src/component/Todo.js
@@ -0,0 +1,41 @@
+import React  from "react";
+import { useRef, useState } from "react";
+
+function Todo(){
+    const userR = useRef(null);
+    const arr1 = [];
+    const [arr, setarr] = useState(arr1);
+  
+    function clear() {
+      setarr([]);
+    }
+  
+    function add() {
+      const newValue = userR.current.value;
+      setarr((prevArr) => [...prevArr, newValue]);
+      userR.current.value = ""; // Clear the input field after adding
+    }
+  
+    return (
+
+      <div>
+      
+       <input ref={userR} type="text" placeholder="enter data to insert" />
+        <button onClick={add}>add</button>
+        
+        {arr.map((ele, index) => (
+          <h1 key={index}>{ele} <button onClick={clear}>clear</button></h1>
+        ))}
+       
+        
+        
+      </div>
+    );
+  
+}
+export default Todo;
+
+
+
+
+
