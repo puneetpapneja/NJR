c5640db12d2ee188a24549b3add713e9df29bc18
kanishka-parmar <kanishkaparmar2004@gmail.com>
2023-08-11 00:14:42 +0530

    register page

--- project/jobportal/src/components/register.js
+++ project/jobportal/src/components/register.js
@@ -3,13 +3,13 @@ import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import {Link} from 'react-router-dom';
 
-const Login = ()=>{
+const Register = ()=>{
 
 return (
 <div className="form-container">
 <Form className="form">
 <div className="form-content">
-  <h1 className="form-title">Login</h1>
+  <h1 className="form-title">Register</h1>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
@@ -18,20 +18,32 @@ return (
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicRadio">
+    <Form.Check
+            inline
+            label="Job Seeker"
+            id={"Job seeker"}/>
+     <Form.Check
+            inline
+            label="Job Recruiter"
+            id={"Job Recruiter"}
+          />
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicCheckbox">
   <Form.Text className="text-muted">
-  <Card.Link as={Link} to="/register">Don't have a account?Register Now</Card.Link>
+  <Card.Link as={Link} to="/">Have a account?Login Now</Card.Link>
         </Form.Text>
   </Form.Group>
 
   <Button variant="dark" type="submit" className="btn">
-    Login
+    Register
   </Button>
   </div>
 </Form>
 </div>
 )
 }
-export default Login;
+export default Register;
--- project/jobportal/src/routing.js
+++ project/jobportal/src/routing.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Loginpage from './pages/loginpage';
+import Registerpage from './pages/registerpage';
 
 export const routing = [
     {
         path: "/",
         element:  <Loginpage />
+    },
+    {
+        path: "/register",
+        element:  <Registerpage />
     }
 ]
 
