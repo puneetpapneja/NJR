d70498771f9137f6549c7123c6b93f7cc332ab46
sumit.vasita <sumitvasita5@gmail.com>
2023-08-10 13:48:03 +0530

    folders

--- practice/sumit/src/index.js
+++ practice/sumit/src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(
