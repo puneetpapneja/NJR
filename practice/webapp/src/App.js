af5b2aa9fe84580cbc5543e58f62fb0f39ab1c87
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-08-11 13:14:54 +0530

    files added

--- practice/webapp/src/App.js
+++ practice/webapp/src/App.js
@@ -1,18 +1,21 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import Login from "./components/Login";
+import './components/Login.css'
+   
 
 // import './components/Item.css';
 // import Item from './components/Item';
-import TodoList from './components/TodoList';
-import './components/TodoList.css'
+// import TodoList from './components/TodoList';
+// import './components/TodoList.css'
 
 
 function App() {
   return (
     <div className="App">
       {/* <Item/> */}
-      <TodoList/>
+      <Login/>
     </div>
   );
 }
