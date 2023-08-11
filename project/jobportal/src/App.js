9a0f3da3d6e072ab05a42f40d8ba2061ce899ab0
YASH-1503 <jainy1601@gmail.com>
2023-08-11 11:16:56 +0530

    commiting projectfiles

---
+++ project/jobportal/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
