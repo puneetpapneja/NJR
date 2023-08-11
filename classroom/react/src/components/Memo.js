4e73b0248e8474b5a152027f9b143e4bc626d455
sahil19solanki <sahil19solanki@gmail.com>
2023-08-11 20:13:42 +0530

    added props and useState

--- classroom/react/src/components/Memo.js
+++ classroom/react/src/components/Memo.js
@@ -1,7 +1,8 @@
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Memo = () => {
+<<<<<<< HEAD
   const [number, setNumber] = useState();
   const [counter, setCounter] = useState(0);
   const handleClick = () => setCounter(counter + 1);
@@ -41,6 +42,48 @@ const Memo = () => {
           </Button>
         </Col>
       </Row>
+=======
+    const [number, setNumber] = useState();
+    const [counter, setCounter] = useState(0);
+    const refNumber = useRef();
+    const handleClick = () => {
+        console.log("ref", refNumber);
+        setCounter(counter + 1);
+    }
+
+    const handleChange = (event) => setNumber(event.target.value)
+    const getSquare = () => {
+        console.log("getSquare called");
+        return parseInt(number) * 2;
+    }
+    const square = useMemo(() => getSquare(),[number]);
+
+    return (<Container fluid>
+        <Row>
+            <Col><h1>useMemo Example</h1></Col>
+        </Row>
+        <Row>
+            <Col>
+                Number : {number || 'Not yet entered'}   <br />
+                Counter: {counter || 'Not yet clicked'} <br />
+                square : {square}
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={6}>
+                <Form.Control
+                    ref={refNumber}
+                    type="text"
+                    id="inputPassword5"
+                    aria-describedby="passwordHelpBlock"
+                    onChange={(event) => handleChange(event)}
+                />
+            </Col>
+            <Col xs={6}>
+            <Button variant="primary" onClick={handleClick}>click</Button>
+            </Col>
+        </Row>
+>>>>>>> aaf16160e37564c86318ec8c804ce6c87e55058c
     </Container>
   );
 };
