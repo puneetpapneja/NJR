cdc9c8c62d1ad595c11d3e5ae54b5c29c6eca92d
Rupalsoni123 <rupalsoni518@gmail.com>
2023-08-11 23:21:03 +0530

    register page

--- project/jobportal/src/App.js
+++ project/jobportal/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard'
 function App() {
   return (
     <>
-    <Login></Login>
+    <Dashboard/>
     </>
   );
 }
