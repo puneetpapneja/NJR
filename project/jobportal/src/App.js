65a5cf4796335876c0f861c8514531a2d704344e
Maa@183 <Maa@183>
2023-08-12 09:13:08 +0530

    commit

--- project/jobportal/src/App.js
+++ project/jobportal/src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-   <Register/>
+   <Dashboard/>
   );
 }
 
---
+++ project/jobportal/src/components/dashboard.js
@@ -0,0 +1,43 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavScrollExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' ,width: '400px'}}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Post A job</Nav.Link>
+            <Nav.Link href="#action3">Applied job</Nav.Link>
+            <Nav.Link href="#action4">Posted job</Nav.Link>
+           
+              
+            
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+  );
+}
+
+export default NavScrollExample;
