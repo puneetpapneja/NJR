54ae20ea8a6576fcb5446f6e71c579d57b7474bd
Khushigahlot29 <gahlotkhushi5@gmail.com>
2023-08-12 09:41:30 +0530

    jobportal files

--- practice/webapp/src/App.js
+++ practice/webapp/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Item from './component/Item';
-function App() {
-  return  (<div>
-    <Item/>
+//import logo from './logo.svg';
+//import './App.css';
+//import Item from './component/Item';
+//function App() {
+ // return  (<div>
+   // <Item/>
     
+    //</div>
+ // );
+//}
+
+
+import React from 'react';
+import LoginForm from './component/login';
+
+function App() {
+  return (
+    <div className="App">
+      < LoginForm/>
     </div>
   );
 }
