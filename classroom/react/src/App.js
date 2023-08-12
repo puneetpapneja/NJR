fe253f93971e022a9db4f4ee9a5bf3e7acce02bf
dsd739 <dsd739@gmail.com>
2023-08-12 09:36:59 +0530

    Changes to be committed: 	modified:   classroom/react/src/App.js 	new file:   classroom/react/src/components/Basicexample.jsx 	new file:   classroom/react/src/components/Login.css

--- classroom/react/src/App.js
+++ classroom/react/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routing';
+import Login from './components/Basicexample';
 
 
 function App() {
   return (
+    <>
+    <Login/>
     <RouterProvider router={createBrowserRouter(routes)}>
     </RouterProvider>);
+    </>
 }
 
 export default App;
