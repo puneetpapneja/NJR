feb54fa6ccad662ef9d0e5d6072d639cb4f221cb
Maa@183 <Maa@183>
2023-08-13 09:39:46 +0530

    commit

--- project/jobportal/src/components/dashboard.js
+++ project/jobportal/src/components/dashboard.js
@@ -4,40 +4,45 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function NavScrollExample() {
+function Dashboard() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+   <container>
+     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand href="#">Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
+        <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' ,width: '400px'}}
+            style={{ maxHeight: '100px' }}
             navbarScroll
           >
+        
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Post A job</Nav.Link>
-            <Nav.Link href="#action3">Applied job</Nav.Link>
-            <Nav.Link href="#action4">Posted job</Nav.Link>
-           
-              
-            
-          </Nav>
-          <Form className="d-flex">
+            <Nav.Link href="#action2">Post A Job</Nav.Link>
+            <Nav.Link href="#action1">Applied job</Nav.Link>
+            <Nav.Link href="#action2">Posted job</Nav.Link>
+
+            <Form className="d-flex">
             <Form.Control
               type="search"
               placeholder="Search"
-              className="me-2"
+              className="ms-25rem"
               aria-label="Search"
             />
+           
           </Form>
-        </Navbar.Collapse>
+         
+
+         
+       </Nav>
       </Container>
     </Navbar>
-    
+   </container>
+  
+
   );
 }
 
-export default NavScrollExample;
+export default Dashboard
