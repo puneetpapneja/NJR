2a29383c3cf1182933fd0cabd3e0c2670fc15303
jainiljain31 <jainiljain31@gmail.com>
2023-08-14 10:16:35 +0530

    3.0

---
+++ Project/jobportal/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+function App() {
+  return (
+    <>
+    <Login></Login>
+    </>
+  );
+}
+
+export default App;
