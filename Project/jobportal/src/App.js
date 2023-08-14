d293a04a27e8d9d403feb5bd5dd0c14411e0609e
suyashsonii <suyashsoni1290@gmail.com>
2023-08-14 10:16:05 +0530

    msg

--- Project/jobportal/src/App.js
+++ Project/jobportal/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <Login />;
-      <Register />
+      {/* <Login />;
+      <Register />; */}
+      <NavBar />;
     </div>
   );
 }
