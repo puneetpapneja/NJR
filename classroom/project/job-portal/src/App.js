c73dea17995f3d71eb06fc8efd61ba0c53778062
Kashvi Pandey <kashvikv2@gmail.com>
2023-08-16 17:56:01 +0530

    unused imports removed

--- classroom/project/job-portal/src/App.js
+++ classroom/project/job-portal/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { routes } from "./routing";
--- classroom/project/job-portal/src/components/AppliedJobs.jsx
+++ classroom/project/job-portal/src/components/AppliedJobs.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Form } from "react-router-dom";
-import SearchBar from "./SearchBar";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import jobData from "./resources/jobDetailData.json";
