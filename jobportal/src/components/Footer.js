314f157348f5c95e2d31072d95df76d5103c1a7c
kunalll191 <kunalsharma1914333@gmail.com>
2023-08-16 15:49:43 +0530

    dashboard incomplete

---
+++ jobportal/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../index.css";
+import { Container } from 'react-bootstrap';
+
+function Footer() {
+    return ( <
+        div className = 'footer' >
+        <
+        div className = 'footertext' > TM - Techno India NJR Institite of Techology - Kunal Sharma < /div> <
+        /div>
+    );
+}
+
+export default Footer;
