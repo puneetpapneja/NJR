9c7edd7b3dc381daf6f6fd38e7bb82851a9cd603
menaria_chinmay <chinmaymenaria827@gmail.com>
2023-08-16 15:17:14 +0530

    dashboard copy

---
+++ jobportal/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router,Routes, Route,} from 'react-router-dom';
+import RegisterPage from './pages/registerpage.js';
+import LoginPage from './pages/loginpage.js';
+import Dashboard from './pages/Dashboardpage.js';
+import PostJob from './components/Postjob.js';
+import Navbar1 from './components/Navbar.js';
+import Footer from './components/Footer.js';
+import React from 'react';
+import Register from './components/Register.js';
+import Dashboardpage from './pages/Dashboardpage.js';
+function App() {
+  return (  
+<React.Fragment>
+    
+    <Router>
+    <Navbar1/>
+      <Routes>
+        <Route exact path='/login' element={<LoginPage/>}></Route>
+        <Route exact path='/register' element={<RegisterPage/>}></Route>
+        <Route exact path='/dashboard' element={<Dashboardpage/>}></Route>
+        <Route exact path='/postjob' element={<PostJob/>}></Route>
+
+      </Routes>
+      <Footer/>
+      
+    </Router>
+    
+    </React.Fragment>
+    
+  );
+}
+export default App;
