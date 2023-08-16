01d3ef6a9d8ddf9bbf42f064f649b97e80a9f150
Vishal kumawat <kumawatvishal0376@gmail.com>
2023-08-16 13:38:27 +0530

    done

--- practice/project/src/App.js
+++ practice/project/src/App.js
@@ -4,19 +4,23 @@ import Navbar from './components/Navbar.js';
 import Login from "./components/login.js";
 import Register from "./components/Register.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/footer';
+import Cards from "./components/Cards";
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-            <Navbar/>
+        <Navbar />
         <div>
           <Routes>
+            <Route path='/Home' element={<Cards/>} />
             <Route path='/Login' element={<Login />} />
             <Route path='/Register' element={<Register />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
--- practice/project/src/components/Navbar.js
+++ practice/project/src/components/Navbar.js
@@ -4,14 +4,14 @@ const Navbar = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/">Navbar</a>
+                <NavLink className="navbar-brand" to="/Home">Navbar</NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <NavLink className="nav-link" to="/">Home</NavLink>
+                            <NavLink className="nav-link" to="/Home">Home</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/login">Login</NavLink>
@@ -19,6 +19,9 @@ const Navbar = () => {
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/Register">Register</NavLink>
                         </li>
+                        {/* <li className="nav-item">
+                            <NavLink className="nav-link" to="/Cards">Cards</NavLink>
+                        </li> */}
                         {/* <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Dropdown
