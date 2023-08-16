20fa377d108e3b477a2d41f658c62205f56138ff
Jaydeep Dangi <jaydeepdangi3@gmail.com>
2023-08-16 15:14:31 +0530

    dashboard incomplete

---
+++ project/jobportal/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../index.css";
+import { Container } from 'react-bootstrap';
+
+function Footer() {
+  return (
+    <div className='footer'>
+    <div className='footertext'>TM-Techno India NJR Institite of Techology-Jaydeep Dangi</div>
+  </div>
+  );
+}
+
+export default Footer;
