2942c28ecaba3237e9faaf7fcd6727fe172955bb
Hetal0210 <hetalsharma2002@gmail.com>
2023-08-16 15:51:50 +0530

    project

---
+++ Project/jobportal (1)/jobportal/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../index.css";
+import { Container } from 'react-bootstrap';
+
+function Footer() {
+  return (
+    <div className='footer'>
+    <div className='footertext'>TM-Techno India NJR Institite of Techology-Hetal</div>
+  </div>
+  );
+}
+
+export default Footer;
