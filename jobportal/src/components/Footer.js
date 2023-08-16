4f0d1056fd6f2b3e242d0a6a724f0585da6aedf4
kunalmenaria <kunalmenaria.2021cs@technonjr.org>
2023-08-16 15:52:42 +0530

    update

---
+++ jobportal/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../index.css";
+import { Container } from 'react-bootstrap';
+
+function Footer() {
+  return (
+    <div className='footer'>
+    <div className='footertext'>TM-Techno India NJR Institite of Techology-kunal menaria</div>
+  </div>
+  );
+}
+
+export default Footer;
