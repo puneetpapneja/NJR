9cdd60da8545a08b7806a4f124ae65b2a0b01e59
bhavya-mehta <bhavyamehta4439@gmail.com>
2023-08-17 10:32:36 +0530

    changes

--- project/jobportal/src/pages/loginpage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Login from '../components/login';
-const loginpage = ()=>{
-
-    return <Login />
-}
-export default loginpage;
--- project/jobportal/src/pages/registerpage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Register from '../components/register';
-const registerpage = ()=>{
-    
-    return <Register />
-}
-export default registerpage;
