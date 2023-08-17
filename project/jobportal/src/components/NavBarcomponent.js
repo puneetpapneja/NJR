f5b92f80c816cc79da5f301cf395ffc804daa5fe
sumit.vasita <sumitvasita5@gmail.com>
2023-08-17 15:30:08 +0530

    commited

--- project/jobportal/src/components/NavBarcomponent.js
+++ project/jobportal/src/components/NavBarcomponent.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { Navbar, Nav, Container, FormControl, Button, InputGroup } from 'react-bootstrap';
+import { Container, Nav, Navbar, Form, FormControl, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const NavBarcomponent = ()=>{
+const NavBarComponent = () => {
   return (
-  <h1>Hello</h1>
+    <Navbar bg="light" expand="lg">
+       <div style={{ width: '10px' }}></div> 
+      <Navbar.Brand href="/Dashboard" className="col-lg-4">
+        Logo
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarNav" />
+      <Navbar.Collapse id="navbarNav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/Dashboard">Home</Nav.Link>
+          <Nav.Link href="/Jobs">Jobs</Nav.Link>
+          <Nav.Link href="/Postjob">Post a Job</Nav.Link>
+          <Nav.Link href="#">Applied Jobs</Nav.Link>
+          <Nav.Link href="#">Posted Jobs</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="search" placeholder="Search" className="mr-sm-2" />
+        </Form>
+        <Nav.Link href="#" className="col-xl-5 d-flex justify-content-end">
+          <i style={{ fontSize: "40px" }} className="bi bi-person-circle"></i>
+        </Nav.Link>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default NavBarcomponent;
+export default NavBarComponent;
--- project/jobportal/src/components/pagelayout.js
+++ project/jobportal/src/components/pagelayout.js
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import React from 'react';
-import NavBarcomponent from "./NavBarcomponent";
+import NavBarcomponent from "./NavBarComponent";
+import Footercomponent from "./footer.js";
 
 const Pagelayout = ()=>{
     return (
         <React.Fragment>
             <NavBarcomponent />
             <Outlet />
+            <Footercomponent />
         </React.Fragment>
     )
 }
