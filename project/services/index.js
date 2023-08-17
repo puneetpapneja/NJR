89a5ac406b3511ba77e4498b44ac366639f5aae3
kanishka-parmar <kanishkaparmar2004@gmail.com>
2023-08-17 20:34:26 +0530

    API example

--- project/services/index.js
+++ project/services/index.js
@@ -1,3 +1,10 @@
 const express = require("express");
+const port =require("./config");
+
+
 const app = express();
 app.use(express.json());
+app.get("/health",(req,res)=>{
+    res.send({status: "OK"});
+});
+app.listen(port,()=> console.log("services started on port: ",port));
