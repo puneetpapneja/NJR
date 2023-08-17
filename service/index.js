a69ac4de8b3b0c740d4043b6d2240478c84f37d8
bhavya-mehta <bhavyamehta4439@gmail.com>
2023-08-17 10:18:55 +0530

    changes

--- service/index.js
+++ service/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const { port } = require('./config');
+const db = require('./database');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-const db = require('./database')
-
 app.use(express.json());
 // const port = 5000;
-
+app.use("/user", userRoutes);
 app.get("/health",(req, res)=>{
     res.send({status: "OK"});
-})
+});
 
-app.listen(port,() => console.log("Service started.",port));
+
+app.listen(port,() => console.log("Service started on port:",port));
