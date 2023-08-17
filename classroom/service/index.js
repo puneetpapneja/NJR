af41887dbea173643aba6fcf8dfb2ec9c5035a05
SK-x7 <111245838+SK-x7@users.noreply.github.com>
2023-08-17 09:31:29 +0530

    service file added

---
+++ classroom/service/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { port } = require("./config");
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req, res)=>{
+res.send({status: "OK"});
+})
+
+app.listen(port,() => console.log("service started", port));
