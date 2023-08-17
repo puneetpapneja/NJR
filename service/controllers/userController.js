76e2191831857b88e3c74f6dd503a2df0ac0a946
bhavya-mehta <bhavyamehta4439@gmail.com>
2023-08-17 10:02:34 +0530

    changes

---
+++ service/controllers/userController.js
@@ -0,0 +1,10 @@
+const userModel = require('../models/userModel');
+
+module.exports = {
+    create: (req, res) => {
+        //logic/data validation code write here
+        return userModel.create(req.body, res);
+    }
+
+
+}
