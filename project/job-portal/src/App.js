e23e000266150b00c5f61565edf50c270687169d
bhumivardar <bhumikavardar@gmail.com>
2023-08-17 13:56:20 +0530

    done

---
+++ project/job-portal/src/App.js
@@ -0,0 +1,31 @@
+// / import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar.js';
+import Login from "./components/login.js";
+import Register from "./components/register.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/footer';
+import Cards from "./components/cards";
+import PostJob from './components/postjob';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path='/Home' element={<Cards/>} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/Register' element={<Register />} />
+            <Route exact path='/postjob' element={<PostJob/>}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
