2fbb5a457cd4fb218ce687e7196f02207892d8b0
suyashsonii <suyashsoni1290@gmail.com>
2023-08-18 11:50:42 +0530

    message

--- Project/jobportal/src/App.js
+++ Project/jobportal/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { routes } from "./Routing";
+import { routes } from "./routing";
 
 function App() {
   return <RouterProvider router={createBrowserRouter(routes)}></RouterProvider>;
