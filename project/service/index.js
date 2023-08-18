9c44db1c67d8f5ac1ef4f96925d20810d6becc82
tanishakumawat <113818130+tanishakumawat@users.noreply.github.com>
2023-08-18 11:43:05 +0530

    modified:   project/service/index.js

--- project/service/index.js
+++ project/service/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const {port} = require("./config.js");
-const db=require("./database.js");
+const { port } = require("./config");
+const db = require('./database');
+const jobRoute = require('./routes/jobRoute');
+
 const app = express();
+
 app.use(express.json());
-app.get("/health",(req,res)=>{
-    res.send({status:"ok"});
-})
-const port=5000;
-app.listen(port,()=> Console.log("service started on port:",port));
+app.use("/job",jobRoute);
+app.get("/health",(req, res)=>{
+res.send({status: "OK"});
+});
+
+
+
+app.listen(port,() => console.log("service started on port: ", port));
