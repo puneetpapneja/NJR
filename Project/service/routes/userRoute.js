3eeb7edad18e299655b867e4575b119219a2886c
suyashsonii <suyashsoni1290@gmail.com>
2023-08-20 21:25:56 +0530

    msg

--- Project/service/routes/userRoute.js
+++ Project/service/routes/userRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/create", userController.create);
-router.post("/getAll", userController.getAll);
+router.get("/getAll", userController.getAll);
 router.delete("/deleteById", userController.deleteById);
 router.put("/update", userController.update);
 
