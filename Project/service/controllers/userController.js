fe8572814be097a4e2c91dc8c5e918bd8931acc9
suyashsonii <suyashsoni1290@gmail.com>
2023-08-20 18:28:40 +0530

    message

---
+++ Project/service/controllers/userController.js
@@ -0,0 +1,47 @@
+const userModel = require("../models/userModel");
+
+module.exports = {
+  create: (req, res) => {
+    return userModel
+      .create(req.body)
+      .then((data) => {
+        return res.send({
+          status: "ok",
+          msg: "User created successfully.",
+          data: data,
+        });
+      })
+      .catch((err) => {
+        return res.send({ status: "fail", error: err });
+      });
+  },
+  getAll: (req, res) => {
+    return userModel
+      .getAll()
+      .then((allUsers) => res.send(allUsers))
+      .catch((err) => res.send({ status: "fail", error: err, code: 500 }));
+  },
+  deleteById: (req, res) => {
+    return userModel.deleteById(req.body.id).then((deletedUser) =>
+      res
+        .then({
+          status: "OK",
+          msg: "User deleted successfully",
+          deletedUser: deletedUser,
+        })
+        .catch((err) => res.send({ status: "fail", error: err }))
+    );
+  },
+  update: (req, res) => {
+    return userModel
+      .update(req.body.id, req.body.fields)
+      .then((updatedUser) =>
+        res.send({
+          status: "ok",
+          msg: "User updated successfully.",
+          updatedUser: updatedUser,
+        })
+      );
+      .catch((err)=> res.send({status:"fail", error: err}));
+  },
+};
