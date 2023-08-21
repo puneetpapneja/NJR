a2e8708511fe824780842db7ad61943d76b9daa3
unknown <abipra2003@gmail.com>
2023-08-21 15:07:09 +0530

    done

---
+++ project/service/Route/userRoute.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../Controller/userController');
+
+router.post('/create', userController.create);
+router.get('/getAll', userController.getAll);
+router.delete('/deleteById', userController.deleteById);
+router.put('/update', userController.update);
+module.exports = router;
