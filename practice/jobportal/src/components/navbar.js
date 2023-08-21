9dec6c8f13d24819f592742da54e703d93d81036
Vishal kumawat <kumawatvishal0376@gmail.com>
2023-08-21 14:58:22 +0530

    done

--- practice/jobportal/src/components/navbar.js
+++ practice/jobportal/src/components/navbar.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Nav, Navbar, Form, FormControl} from 'react-bootstrap';
-import { NavLink,Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const NavBarComponent = () => {
   return (
     <Navbar bg="light" expand="lg">
       <div style={{ width: '10px' }}></div>
-      <Navbar.Brand href="/Dashboard" className="col-lg-4">
-        Logo
+      <Navbar.Brand as={Link} to="/Dashboard" className="col-lg-4">
+        Home
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarNav" />
       <Navbar.Collapse id="navbarNav">
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/Dashboard">Home</Nav.Link>
+          {/* <Nav.Link as={Link} to="/Dashboard">Home</Nav.Link> */}
           <Nav.Link as={Link} to="/Jobs">Jobs</Nav.Link>
           <Nav.Link as={Link} to="/Postjob">Post a Job</Nav.Link>
           <Nav.Link as={Link} to="#">Applied Jobs</Nav.Link>
@@ -21,8 +21,8 @@ const NavBarComponent = () => {
         <Form inline>
           <FormControl type="search" placeholder="Search" className="mr-sm-2" />
         </Form>
-        <Nav.Link href="#" className="col-xl-5 d-flex justify-content-end">
-          <i style={{ fontSize: "40px" }} className="bi bi-person-circle"></i>
+        <Nav.Link as={Link} to="/LoginPage.js" className="col-xl-5 d-flex justify-content-end">
+          <i style={{ fontSize: "40px" }} className="bi bi-person-circle" as={Link} to="/LoginPage.js"></i>
         </Nav.Link>
       </Navbar.Collapse>
     </Navbar>
