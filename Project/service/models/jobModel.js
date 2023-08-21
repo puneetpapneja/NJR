fce90ba1127455ee52894618077d09053420d3bb
jainiljain31 <jainiljain31@gmail.com>
2023-08-21 09:33:13 +0530

    4.0

---
+++ Project/service/models/jobModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const jobSchema = mongoose.Schema({
+    jobTitle:String,
+    jobDesc:String,
+    maxSalary:String,
+    companyName:String,
+    recruiterDtl:{
+        name : String ,
+        emailId  : String,
+        contactNo:String
+    }
+});
+
+const jobCollection = mongoose.model("jobs",jobSchema);
+
+module.exports = {
+    create: (fields)=>{
+        const user = new jobCollection(fields);
+        return user.save();       
+    },
+    getAll: ()=> jobCollection.find(),
+    deleteById: (id) => jobCollection.deleteOne({_id: id}),
+    update: (id, fields) => jobCollection.updateOne({_id: id}, fields)
+}
