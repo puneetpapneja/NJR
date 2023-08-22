3ceb911ca0f1c7be2fa23b0f59e9eabab9b1e750
sargamjain18 <sargamjain18@gmail.com>
2023-08-22 10:37:47 +0530

    modified:   project/service (modified content, untracked content)

--- project/jobportal/src/pages/Appliedajob.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Applied from '../components/Appliedjob';
-export default function Post()
-{
-    return(<Applied/>);
-}
--- project/jobportal/src/pages/Post.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import PostJob from "../components/Postajob";
-export default function Post()
-{
-    return(<PostJob />);
-}
