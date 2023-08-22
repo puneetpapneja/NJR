6c213ef7c075283b3ada3f24819ace28d9186377
Dhirensuhalka <suhalka.dhiren07@gmail.com>
2023-08-22 11:30:27 +0530

    Changes to be committed: 	modified:   project/jobportal/src/components/Appliedjob.js 	new file:   project/jobportal/src/components/Appliedjob2.js 	new file:   project/jobportal/src/components/Developer.js 	new file:   project/jobportal/src/components/Header.js 	modified:   project/jobportal/src/components/Jobdescription.js 	modified:   project/jobportal/src/components/Navigationbar.js 	new file:   project/jobportal/src/components/Postedjob.js 	new file:   project/jobportal/src/components/Profile.js 	new file:   project/jobportal/src/components/detailsofdashboard.js 	new file:   project/jobportal/src/components/job.js 	new file:   project/jobportal/src/pages/Appliedajob2.js 	new file:   project/jobportal/src/pages/Developers.js 	new file:   project/jobportal/src/pages/Postedajob.js 	new file:   project/jobportal/src/pages/Profiles.js 	modified:   project/jobportal/src/pages/registerpage.js 	modified:   project/jobportal/src/routing.js 	new file:   project/service/controllers/jobController.js 	new file:   project/service/controllers/userController.js 	new file:   project/service/database.js 	modified:   project/service/index.js 	new file:   project/service/models/jobModel.js 	new file:   project/service/models/userModel.js 	modified:   project/service/package-lock.json 	modified:   project/service/package.json 	new file:   project/service/routes/jobRoute.js 	new file:   project/service/routes/userRoute.js

--- project/service/index.js
+++ project/service/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const { port } = require("./config");
+const db = require('./database');
+const userRoute = require('./routes/userRoute');
+const jobRoute = require('./routes/jobRoute');
+
 
 const app = express();
 
 app.use(express.json());
+app.use("/user",userRoute);
+app.use("/job",jobRoute);
 
-app.listen(() => console.log("service started"));
+app.get("/health",(req, res)=>{
+res.send({status: "OK"});
+});
+
+
+
+app.listen(port,() => console.log("service started on port: ", port));
---
+++ project/service/models/jobModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const JobSchema = mongoose.Schema({
+     Job_title:String,
+     description:String,
+     max_salary:Number,
+    recuriterDtl:{
+    name:String,
+    emailId:String,
+    contactNo:Number
+     }
+});
+
+const JobCollection = mongoose.model("Job", JobSchema);
+
+module.exports = {
+    create: (fields, res)=>{
+        const Job = new JobCollection(fields);
+        return Job.save();
+    },
+
+        getAll: ()=> JobCollection.find(),
+       deleteById: (id) => JobCollection.deleteOne({_id: id}),
+    update: (id, fields) => JobCollection.updateOne({_id: id}, fields)
+    }
