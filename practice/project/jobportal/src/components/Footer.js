3c2908f6c82721e120345e2101ee78f0a72c3ed4
gaziaman <amankhaan15@gmail.com>
2023-08-23 13:57:55 +0530

    last commit is done

---
+++ practice/project/jobportal/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../index.css";
+
+function Footer() {
+  return (
+    <div className='footer'>
+    <div className='footertext'>TM-Techno India NJR Institite of Techology-Gazi aman khan</div>
+  </div>
+  );
+}
+
+export default Footer;
