e0534a50d96a6fecf391522312139abd11e66141
arun12329 <132123854+arun12329@users.noreply.github.com>
2023-08-25 09:15:04 +0530

    modified:   Project/jobportal/package-lock.json 	modified:   Project/jobportal/package.json 	deleted:    Project/package-lock.json 	deleted:    Project/package.json 	modified:   Project/service/config.js 	new file:   Project/service/controllers/jobsController.js 	modified:   Project/service/database.js 	modified:   Project/service/index.js 	new file:   Project/service/models/jobsModel.js 	new file:   Project/service/models/userModel.js 	modified:   Project/service/package-lock.json 	modified:   Project/service/package.json 	new file:   Project/service/routes/jobsRoute.js 	new file:   Project/service/routes/userRoute.js

--- Project/service/index.js
+++ Project/service/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const { port } = require("./config");
-
+const db = require("./database");
+const jobRoute = require("./routes/jobsRoute");
+const userRoute = require("./routes/userRoute");
+const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(express.json());
+app.use(cors());
+app.use("/job", jobRoute);
+app.use("/user", userRoute);
+app.get("/health", (req, res) => {
+  res.send({ status: "OK" });
+});
 
-app.listen(port,() => {console.log("service started",port)});
-
+app.listen(port, () => console.log("service started on port: ", port));
