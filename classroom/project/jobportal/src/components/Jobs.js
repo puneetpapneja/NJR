049f7fdf3dee5360b79df67159ffd8450bf6fa24
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-08-25 09:30:09 +0530

    changes made

--- classroom/project/jobportal/src/components/Jobs.js
+++ classroom/project/jobportal/src/components/Jobs.js
@@ -1,28 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Form } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CardHeader from "react-bootstrap/esm/CardHeader";
+import FormControl from "react-bootstrap/FormControl";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllJobs, reset } from "../store/reducers/jobSlice";
 
 export default function Jobs() {
   const jobs = useSelector((state) => state?.job?.jobs);
   const dispatch = useDispatch();
-  console.log(jobs);
+
   useEffect(() => {
     dispatch(getAllJobs());
-  }, [dispatch]);
-
-  useEffect(() => {
     return () => {
       dispatch(reset());
-    };
-  }, [dispatch]);
+    };
+  }, [dispatch]);
 
-  const renderJobs = () => {
-    return jobs.map((items) => (
-      <Card className="col-md-10 mt-4 m-auto">
+  const renderJobs = (jobsArray) => {
+    return jobsArray.map((items) => (
+      <Card className="col-md-10 mt-4 m-auto" key={items.id}>
         <Card.Header as="h5" className="d-flex justify-content-between" inline>
           <span>{items.title}</span>
           <>{items.maxSalary}</>
@@ -35,12 +33,38 @@ export default function Jobs() {
       </Card>
     ));
   };
+
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredJobs = jobs.filter((item) =>
+    item.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <Form>
       <CardHeader style={{ fontSize: "50px", marginLeft: "8vw" }}>
         Jobs
-      </CardHeader>{" "}
-      {renderJobs()}
+        <Form className="d-flex mt-3">
+          <FormControl
+            type="search"
+            placeholder="🔍 Search"
+            className="me-2"
+            aria-label="Search"
+            value={searchText}
+            onChange={handleSearchChange}
+          />
+          <Button variant="outline-primary">🔍</Button>
+        </Form>
+      </CardHeader>
+      {filteredJobs.length > 0 ? (
+        renderJobs(filteredJobs)
+      ) : (
+        <p>No matching jobs found.</p>
+      )}
     </Form>
   );
 }
