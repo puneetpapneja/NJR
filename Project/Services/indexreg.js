5c0c7549510ab3be907e84cd2e2664173a2e05a6
vedas-dixit <vedasdixit1006@gmail.com>
2023-09-07 14:42:24 +0530

    Latest commit

--- Project/Services/indexreg.js
+++ Project/Services/indexreg.js
@@ -9,38 +9,64 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/demo');
   console.log('db connected')
 }
+
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    userType: String, // Add userType field
 });
 
 const User = mongoose.model('User', userSchema);
-
-
-
 
 const server = express();
 
 server.use(cors());
 server.use(bodyParser.json());
 
-// CRUD - Create
-server.post('/demo',async (req,res)=>{
-     
+server.post('/demo', async (req, res) => {
     let user = new User();
     user.username = req.body.username;
     user.password = req.body.password;
+    user.userType = req.body.userType; // Save userType
     const doc = await user.save();
 
     console.log(doc);
     res.json(doc);
-})
+});
 
-server.get('/demo',async (req,res)=>{
+server.get('/demo', async (req, res) => {
     const docs = await User.find({});
     res.json(docs)
-})
+});
 
-server.listen(8080,()=>{
+
+// server.get('/demo', async (req, res) => {
+//     const username = req.query.username; // Assuming you're passing the username as a query parameter
+//     const user = await User.findOne({ username });
+//     if (user) {
+//         res.json({ userType: user.userType });
+//     } else {
+//         res.status(404).json({ error: 'User not found' });
+//     }
+// });
+
+
+
+server.get('/userType/:username', async (req, res) => {
+    const username = req.params.username;
+    const user = await User.findOne({ username });
+    if (user) {
+        res.json({ userType: user.userType });
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+});
+
+
+
+
+
+
+server.listen(7070, () => {
     console.log('server started')
-})
+});
