b2cf13fb2591992dec17ed9cd1e59c47ec9a5251
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-09-07 10:52:48 +0530

    job posting notification added

--- classroom/project/jobportal/src/components/postjob.js
+++ classroom/project/jobportal/src/components/postjob.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Toast } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { postNewJob } from "../store/reducers/postJobSlice";
 
@@ -18,54 +18,74 @@ export default function PostJob() {
 
   const dispatch = useDispatch();
   const postingStatus = useSelector((state) => state.postJob.postingStatus);
+  const [showToast, setShowToast] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title && description && maxSalary) {
-      // Dispatch the action to post new job
       dispatch(postNewJob({ title, description, maxSalary }));
+      setShowToast(true);
     }
   };
+
   return (
-    <Form style={formStyle} onSubmit={handleSubmit}>
-      <h1 className="mb-4">Post Job</h1>
+    <div>
+      <Form style={formStyle} onSubmit={handleSubmit}>
+        <h1 className="mb-4">Post Job</h1>
 
-      <Form.Group className="mb-3 col-md-4" controlId="jobTitle">
-        <Form.Label>Job Title</Form.Label>
-        {/* <Form.Control type="text" placeholder="Enter job title" /> */}
-        <Form.Control
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </Form.Group>
+        <Form.Group className="mb-3 col-md-4" controlId="jobTitle">
+          <Form.Label>Job Title</Form.Label>
+          {/* <Form.Control type="text" placeholder="Enter job title" /> */}
+          <Form.Control
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3 col-md-4" controlId="jobDescription">
-        <Form.Label>Job Description</Form.Label>
-        {/* <Form.Control as="textarea" placeholder="Enter job description" rows={3} /> */}
-        <Form.Control
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </Form.Group>
+        <Form.Group className="mb-3 col-md-4" controlId="jobDescription">
+          <Form.Label>Job Description</Form.Label>
+          {/* <Form.Control as="textarea" placeholder="Enter job description" rows={3} /> */}
+          <Form.Control
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3 col-md-4" controlId="maxSalary">
-        <Form.Label>Max salary</Form.Label>
-        {/* <Form.Control type="number" placeholder="Enter max salary" /> */}
-        <Form.Control
-          type="number"
-          value={maxSalary}
-          onChange={(e) => setMaxSalary(e.target.value)}
-        />
-      </Form.Group>
+        <Form.Group className="mb-3 col-md-4" controlId="maxSalary">
+          <Form.Label>Max salary</Form.Label>
+          {/* <Form.Control type="number" placeholder="Enter max salary" /> */}
+          <Form.Control
+            type="number"
+            value={maxSalary}
+            onChange={(e) => setMaxSalary(e.target.value)}
+          />
+        </Form.Group>
 
-      <Button
-        style={{ backgroundColor: "black", border: "none" }}
-        type="submit"
+        <Button
+          style={{ backgroundColor: "black", border: "none" }}
+          type="submit"
+        >
+          Post
+        </Button>
+      </Form>
+
+      <Toast
+        show={showToast}
+        onClose={() => setShowToast(false)}
+        style={{
+          position: "absolute",
+          top: 20,
+          right: 20,
+          backgroundColor: "green",
+          color: "white",
+        }}
+        delay={3000} // Set the duration for how long the toast will be visible
+        autohide
       >
-        Post
-      </Button>
-    </Form>
+        <Toast.Body>Job posted successfully!</Toast.Body>
+      </Toast>
+    </div>
   );
 }
