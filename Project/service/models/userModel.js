c49d2915695811826060f54d239ad3ef78f959e8
suyashsonii <suyashsoni1290@gmail.com>
2023-09-07 15:01:45 +0530

    message

--- Project/service/models/userModel.js
+++ Project/service/models/userModel.js
@@ -12,13 +12,13 @@ const userSchema = mongoose.Schema({
 const userCollection = mongoose.model("users", userSchema);
 
 module.exports = {
-  // create: (fields) => {
-  //   const user = new userCollection(fields);
-  //   return user.save();
-  // },
-  // getAll: () => userCollection.find(),
-  // deleteById: (id) => userCollection.deleteOne({ _id: id }),
-  // update: (id, fields) => userCollection.updateOne({ _id: id }, fields),
+  create: (fields) => {
+    const user = new userCollection(fields);
+    return user.save();
+  },
+  getAll: () => userCollection.find(),
+  deleteById: (id) => userCollection.deleteOne({ _id: id }),
+  update: (id, fields) => userCollection.updateOne({ _id: id }, fields),
   userCollection,
   hasValidUser: (emailId, pwd) => {
     return userCollection.find({ emailId: emailId, password: pwd });
