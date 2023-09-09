5350d8603cffe1d9e17f34f9419558df45bf7969
Code-Dhruv <106569448+Code-Dhruv@users.noreply.github.com>
2023-09-09 11:32:40 +0530

    User API created

---
+++ project/services/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    emailId: String,
+    password :String,
+    type: String,
+    companyName :String 
+});
+
+const userCollection = mongoose.model("users",userSchema);
+
+module.exports ={
+    create: (fields,res)=>{
+        const user =new userCollection(fields);
+        return user.save();
+    },
+    getAll: ()=> userCollection.find(),
+    deleteById:(id)=> userCollection.deleteOne({_id:id}),
+    update: (id,fields)=>userCollection.updateOne({_id:id},fields),
+    hasValidUser: (emailId, pwd) => {
+        return userCollection.find({emailId: emailId, password: pwd});
+      }
+}
