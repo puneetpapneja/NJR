7aa825cd47c6f594b48f967e4cf09441f4127e0f
avani07joshi <avani2002joshi@gmail.com>
2023-09-09 10:35:06 +0530

    modified:   project/services/models/jobmodel.js

--- project/services/models/jobmodel.js
+++ project/services/models/jobmodel.js
@@ -1,11 +1,25 @@
-const mongoose = require("mongoose");
-
-const jobSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  maxSalary: Number,
+const mongoose = require('mongoose');
+const jobSchema = mongoose.Schema({
+    jobTitle: String,
+    jobDescription: String,
+    maxSalary:Number,
+    companyName:String,
+    recruiterDtl :{
+        name:String,
+        emailId:String,
+        contactNo:Number
+    }
 });
 
-const Job = mongoose.model("Job", jobSchema);
+const jobCollection = mongoose.model("job",jobSchema);
 
-module.exports = Job;
+module.exports = {
+    create: (field,res)=>{
+        const job = new jobCollection(field);
+        return job.save()
+    },
+    getall:()=>jobCollection.find(),
+    deleteById:(id)=>jobCollection.deleteOne({_id:id}),
+    update:(id,field)=>jobCollection.updateOne({_id:id,field}),
+    searchjob:(keyword)=>jobCollection.find({$or:[{jobTitle:/.*keyword*./} ,  {companyName: /.*keyword*./} , {_id:/.*keyword*./}]})
+}
