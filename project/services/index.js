f93df4983cdb98c847373897da68129df5646d85
avani07joshi <avani2002joshi@gmail.com>
2023-09-09 11:25:46 +0530

    new file:   project/services/database.js 	modified:   project/services/index.js 	modified:   project/services/package.json

--- project/services/index.js
+++ project/services/index.js
@@ -1,12 +1,18 @@
-const express = request ("express");
-const PORT = require ("./config")
+const express = require("express");
+const {port} =require("./config");
+const db = require('./database');
+const userRoute=require('./routes/userRoute');
+const jobRoute = require('./routes/jobRoute');
+const cors = require('cors');
+
+
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+app.use("/user",userRoute);
+app.use("/job",jobRoute);
+app.get("/health",(req,res)=>{
+    res.send({status: "OK"});
+});
 
-app.get("/health",(req,res)=>(
-    res.send()
-))
-
-app.listen(PORT,() => console.log("service started",PORT));
-    
+app.listen(port,()=> console.log("services started on port: ",port));
