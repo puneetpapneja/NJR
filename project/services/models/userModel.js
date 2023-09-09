f1b8035921b336f470c2f2c12dd06f4f1181ef1f
avani07joshi <avani2002joshi@gmail.com>
2023-09-09 10:38:21 +0530

    changes in userModel

--- project/services/models/userModel.js
+++ project/services/models/userModel.js
@@ -4,22 +4,22 @@ const userSchema = mongoose.Schema({
     firstName: String,
     lastName: String,
     emailId: String,
-    password: String,
+    password :String,
     type: String,
-    companyName: String
+    companyName :String 
 });
 
-const userCollection = mongoose.model("users", userSchema);
+const userCollection = mongoose.model("users",userSchema);
 
-module.exports = {
-    create: (fields, res)=>{
-        const user = new userCollection(fields);
-        return user.save()
-        .then((data) => {
-            return res.send({status: "ok", msg:"User created successfully."})
-        })
-        .catch((err)=>{
-            return res.send({status: "fail", error: err});
-        })
-    }
+module.exports ={
+    create: (fields,res)=>{
+        const user =new userCollection(fields);
+        return user.save();
+    },
+    getAll: ()=> userCollection.find(),
+    deleteById:(id)=> userCollection.deleteOne({_id:id}),
+    update: (id,fields)=>userCollection.updateOne({_id:id},fields),
+    hasValidUser: (emailId, pwd) => {
+        return userCollection.find({emailId: emailId, password: pwd});
+      }
 }
