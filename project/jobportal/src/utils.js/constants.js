464eea5603f5b5cc76a022cd5a12e9b81df4806f
avani07joshi <avani2002joshi@gmail.com>
2023-09-09 09:56:53 +0530

    new file:   project/jobportal/src/utils.js/constants.js

---
+++ project/jobportal/src/utils.js/constants.js
@@ -0,0 +1,25 @@
+export const API_URL = "http://localhost:5000/";
+export const JobSeeker = "Job Seeker"
+export const JobRecruiter = "Job Recruiter"
+
+export const seekerMenu = [
+    {
+        path:"/joblist",
+        name:"Jobs"
+    },
+    {
+        path:"/appliedjob",
+        name:"Applied Job"
+    }
+]
+
+export const recruiterMenu = [
+    {
+        path:"/postjob",
+        name:"Post A Job"
+    },
+    {
+        path:"/postedjob",
+        name:"Posted Job"
+    }
+    ]
