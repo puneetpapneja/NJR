16b7b42836692db49f901fdf29b1395a88584d8b
anjalisoni12 <sonianjaliu@gmail.com>
2023-09-09 10:00:48 +0530

    modified:   project/jobportal/src/components/navbar.js

--- project/jobportal/src/components/navbar.js
+++ project/jobportal/src/components/navbar.js
@@ -2,33 +2,45 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Col,Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { recruiterMenu,seekerMenu } from '../utils/constants';
 
 function Navbarbody() {
+  const hasRecruiter = useSelector(state=>state?.user?.hasRecruiter);
+  const renderNavItems=()=>{
+    const navItems = hasRecruiter ? recruiterMenu : seekerMenu;
+    return navItems?.map(item =>  <Nav.Link as={Link} to={item.path}>{item.name}</Nav.Link>
+    )}
+  
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand>Logo</Navbar.Brand>
           <Nav className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-          >
+            style={{ maxHeight: '50px' }}
+          > <Navbar.Collapse id="basic-navbar-nav">
             <Nav.Link as={Link} to="/dashboard">Home</Nav.Link>
-            <Nav.Link as={Link} to="/postjob">Post a Job</Nav.Link>
-            <Nav.Link as={Link} to="/appliedjob">Applied Job</Nav.Link>
-            <Nav.Link as={Link} to="/postesjob">Postes Job</Nav.Link>
-            
-         
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
-          <div className="mb-2">
-          <i class="bi bi-person-circle"></i>
-          </div>
+            {renderNavItems()}  
+            <Nav.Link as={Col} lg="5" style={{marginLeft:"520px"}} className=".justify-content-sm-end"><Form.Control type="text" placeholder="Search" /></Nav.Link>
+            </Navbar.Collapse>
+            <Nav.Link className=".justify-content-sm-end">
+          <Dropdown>
+            <Dropdown.Toggle variant="link" id="dropdown-basic" style={{marginRight:"30px"}}>
+              <i style={{ fontSize: "30px" }} className="bi bi-person-circle"></i>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to="/profilepage">
+                  Profile 
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/login">
+                 Logout 
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav.Link>
+      
         </Nav>
       </Container>
     </Navbar>
