a84623b6847b7071a9a46ce34de90417e12cf7db
Code-Dhruv <106569448+Code-Dhruv@users.noreply.github.com>
2023-09-09 12:25:57 +0530

    Index updates

--- project/jobportal/src/store/index.js
+++ project/jobportal/src/store/index.js
@@ -1,7 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
-import jobSlice from './reducers/jobslice';
+import jobSlice from '../store/reducers/jobSlice'
 import userSlice  from './reducers/userSlice';
-import notificationSlice from './reducers/notificationslice';
+import notificationSlice from '../store/reducers/notificationSlice';
 
 export const store = configureStore({
     reducer:{
