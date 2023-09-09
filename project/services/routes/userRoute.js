43a3dfef72d5d6033dd9def6f79ba67e9a2be591
avani07joshi <avani2002joshi@gmail.com>
2023-09-09 11:03:19 +0530

    modified:   project/services/routes/userRoute.js

--- project/services/routes/userRoute.js
+++ project/services/routes/userRoute.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express =require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/userControllers');
 
 router.post('/create', userController.create);
+router.get('/getAll',userController.getAll);
+router.delete('/deleteById',userController.deleteById);
+router.put('/update',userController.update);
+router.post('/hasValidUser', userController.hasValidUser);
 
-module.exports = router;
+module.exports =router;
